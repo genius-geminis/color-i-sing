b0a4c17674edb5ccb840462ae8e2113432d2c900
Alena Ioffe <alena.ioffe@gmail.com>
2019-06-26 16:23:01 -0400

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // @ts-nocheck
 let heading = document.querySelector('h1');
-heading.textContent = 'CLICK ANYWHERE TO START'
+heading.textContent = 'CLICK ANYWHERE TO START';
 document.body.onclick = init;
-
 
 // eslint-disable-next-line max-statements
 function init() {
@@ -13,34 +12,36 @@ function init() {
     navigator.mediaDevices = {};
   }
 
-
   // Some browsers partially implement mediaDevices. We can't just assign an object
   // with getUserMedia as it would overwrite existing properties.
   // Here, we will just add the getUserMedia property if it's missing.
   if (navigator.mediaDevices.getUserMedia === undefined) {
     navigator.mediaDevices.getUserMedia = function(constraints) {
-
       // First get ahold of the legacy getUserMedia, if present
-      let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+      let getUserMedia =
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
 
       // Some browsers just don't implement it - return a rejected promise with an error
       // to keep a consistent interface
       if (!getUserMedia) {
-        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        return Promise.reject(
+          new Error('getUserMedia is not implemented in this browser')
+        );
       }
 
       // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
       return new Promise(function(resolve, reject) {
         getUserMedia.call(navigator, constraints, resolve, reject);
       });
-    }
+    };
   }
   // window. is needed otherwise Safari explodes
 
   let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   let source;
   let stream;
-
 
   //set up the different audio nodes we will use for the app
 
@@ -53,7 +54,6 @@ function init() {
   let gainNode = audioCtx.createGain();
   let biquadFilter = audioCtx.createBiquadFilter();
   let convolver = audioCtx.createConvolver();
-
 
   const colors = {
     0: 'red',
@@ -119,90 +119,89 @@ function init() {
     60: 'yellow',
   };
 
-
   // set up canvas context for visualizer
 
   let canvas = document.querySelector('.visualizer');
-  let canvasCtx = canvas.getContext("2d");
+  let canvasCtx = canvas.getContext('2d');
 
   let intendedWidth = document.querySelector('.wrapper').clientWidth;
 
   canvas.setAttribute('width', intendedWidth);
-
 
   let drawVisual;
 
   //main block for doing the audio recording
 
   if (navigator.mediaDevices.getUserMedia) {
-     console.log('getUserMedia supported.');
-     let constraints = {audio: true}
-     navigator.mediaDevices.getUserMedia(constraints)
-        .then(
-          function(stream) {
-             source = audioCtx.createMediaStreamSource(stream);
-             source.connect(distortion);
-             distortion.connect(biquadFilter);
-             biquadFilter.connect(gainNode);
-             convolver.connect(gainNode);
-             gainNode.connect(analyser);
-             analyser.connect(audioCtx.destination);
+    console.log('getUserMedia supported.');
+    let constraints = { audio: true };
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function(stream) {
+        source = audioCtx.createMediaStreamSource(stream);
+        source.connect(distortion);
+        distortion.connect(biquadFilter);
+        biquadFilter.connect(gainNode);
+        convolver.connect(gainNode);
+        gainNode.connect(analyser);
+        analyser.connect(audioCtx.destination);
 
-          	 visualize();
-            //  voiceChange();
-        })
-        .catch( function(err) { console.log('The following gUM error occured: ' + err);})
+        visualize();
+        //  voiceChange();
+      })
+      .catch(function(err) {
+        console.log('The following gUM error occured: ' + err);
+      });
   } else {
-     console.log('getUserMedia not supported on your browser!');
+    console.log('getUserMedia not supported on your browser!');
   }
 
   function visualize() {
     const WIDTH = canvas.width;
     const HEIGHT = canvas.height;
 
-      analyser.fftSize = 2048;
-      let bufferLengthAlt = analyser.frequencyBinCount;
-      console.log(bufferLengthAlt);
-      let dataArrayAlt = new Uint8Array(bufferLengthAlt);
+    analyser.fftSize = 2048;
+    let bufferLengthAlt = analyser.frequencyBinCount;
+    console.log(bufferLengthAlt);
+    let dataArrayAlt = new Uint8Array(bufferLengthAlt);
 
-      // canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    // canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
+    let drawAlt = function() {
+      drawVisual = requestAnimationFrame(drawAlt);
+      analyser.getByteFrequencyData(dataArrayAlt);
+
+      const newArray = dataArrayAlt.slice(0, 60);
+      const newLength = 150;
+
+      let max = 0;
+      for (let i = 0; i < newArray.length; i++) {
+        if (newArray[i] > newArray[max]) {
+          max = i;
+        }
+      }
+      const maxColor = colors[max];
+
+      canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      let barWidth = (WIDTH / newLength) * 2.5;
+      let barHeight;
       let x = 0;
 
-      let drawAlt = function() {
-        drawVisual = requestAnimationFrame(drawAlt);
+      for (let i = 0; i < newLength; i++) {
+        barHeight = newArray[i];
 
-        analyser.getByteFrequencyData(dataArrayAlt);
-
-        const newArray = dataArrayAlt.slice(0, 60);
-        const newLength = 150;
-
-        let max = 0;
-        for (let i = 0; i < newArray.length; i++) {
-          if (newArray[i] > newArray[max]) {
-            max = i;
-          }
-        }
-        const maxColor = colors[max];
-
-        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-        let barWidth = (WIDTH / newLength) * 2.5;
-        let barHeight;
-
-
-        for (let i = 0; i < newLength; i++) {
-          barHeight = newArray[i];
-
-          canvasCtx.fillStyle = maxColor;
-          canvasCtx.fillRect(x, HEIGHT-barHeight/2, barWidth, barHeight/2);
-
-        }
+        canvasCtx.fillStyle = maxColor;
+        canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
         x += barWidth + 1;
+      }
 
-      };
+      for (let j = 0 ; j < )
+      console.log('this is maxColor', maxColor);
+    };
 
-      drawAlt();
-    }
+    drawAlt();
+    // console.log('this is x', x);
+  }
 }
