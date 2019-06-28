68ef8064a32d7ee48a8c011c4c44b6158815210e
Colleen Higgins <cjh323@cornell.edu>
2019-06-28 17:48:43 -0400

    fix (components/draw): removed component mount and update functions

--- client/components/draw.js
+++ client/components/draw.js
@@ -1,6 +1,4 @@
 import React from 'react'
-// import {AudioAnalyser} from './audioAnalyser'
-// import Canvas from './canvas'
 import {colors} from '../../util/colors'
 
 export class Draw extends React.Component {
@@ -11,12 +9,15 @@ export class Draw extends React.Component {
       maxColor: 'white',
       x: 0,
       y: 0,
-      intervalId: '',
       dataUrl: ''
     }
+    this.WIDTH = 500
+    this.HEIGHT = 500
+    this.pixWidth = this.WIDTH / 100
+    this.pixHeight = this.HEIGHT / 100
     this.canvas = React.createRef()
     this.getMic = this.getMic.bind(this)
-    this.gotMic = this.gotMic.bind(this)
+    this.paintNext = this.paintNext.bind(this)
     this.stopMic = this.stopMic.bind(this)
     this.getColor = this.getColor.bind(this)
     this.makePath = this.makePath.bind(this)
@@ -24,90 +25,62 @@ export class Draw extends React.Component {
     this.getImage = this.getImage.bind(this)
   }
 
-  componentDidMount() {
-    const newIntervalId = window.setInterval(() => {
-      this.makePath()
-    }, 50)
-    this.setState({
-      intervalId: newIntervalId
-    })
-  }
-
   async getMic() {
     const audio = await navigator.mediaDevices.getUserMedia({
       audio: true
-      // video: false
     })
-    this.setState({audio})
-    this.gotMic()
-  }
-
-  gotMic() {
     this.audioContext = new (window.AudioContext || window.webkitAudioContext)()
     this.analyser = this.audioContext.createAnalyser()
     this.analyser.fftSize = 2048
     this.dataArray = new Uint8Array(this.analyser.frequencyBinCount)
 
-    if (this.state.audio !== null) {
-      this.source = this.audioContext.createMediaStreamSource(this.state.audio)
+    if (audio) {
+      this.source = this.audioContext.createMediaStreamSource(audio)
       this.source.connect(this.analyser)
-      this.rafId = requestAnimationFrame(this.getColor)
+      this.rafId = requestAnimationFrame(this.paintNext)
     }
+
+    this.setState({audio})
   }
 
   stopMic() {
     this.state.audio.getTracks()[0].stop()
     this.setState({audio: null})
+    cancelAnimationFrame(this.rafId)
     this.getImage()
+  }
+
+  paintNext() {
+    this.getColor()
+    const ctx = this.canvas.current.getContext('2d')
+    ctx.fillStyle = this.state.maxColor
+    ctx.fillRect(this.state.x, this.state.y, 5, 5)
+    ctx.save()
+    this.rafId = requestAnimationFrame(this.paintNext)
+    this.makePath()
   }
 
   getColor() {
     this.analyser.getByteFrequencyData(this.dataArray)
     const newArray = this.dataArray.slice(0, 60)
-
     let max = 0
     for (let i = 0; i < newArray.length; i++) {
       if (newArray[i] > newArray[max]) {
         max = i
       }
     }
-
     const maxColor = colors[max]
     this.setState({maxColor})
-    this.rafId = requestAnimationFrame(this.getColor)
   }
 
   makePath() {
-    const WIDTH = 500
-    const HEIGHT = 500
-    const pixWidth = WIDTH / 100
-    const pixHeight = HEIGHT / 100
-    let newX = this.state.x + pixWidth
+    let newX = this.state.x + this.pixWidth
     let newY = this.state.y
-    if (newX > WIDTH) {
-      newY = this.state.y + pixHeight
+    if (newX > this.WIDTH) {
+      newY = this.state.y + this.pixHeight
       newX = 0
     }
     this.setState({x: newX, y: newY})
-  }
-
-  componentWillUnmount() {
-    cancelAnimationFrame(this.rafId)
-    if (this.analyser && this.source) {
-      this.analyser.disconnect()
-      this.source.disconnect()
-    }
-
-    clearInterval(this.state.intervalId)
-  }
-
-  componentDidUpdate() {
-    if (this.state.audio !== null) {
-      const ctx = this.canvas.current.getContext('2d')
-      ctx.fillStyle = this.state.maxColor
-      ctx.fillRect(this.state.x, this.state.y, 5, 5)
-      ctx.save()
-    }
   }
 
   getImage() {
@@ -120,6 +93,14 @@ export class Draw extends React.Component {
     const context = this.canvas.current.getContext('2d')
     context.clearRect(0, 0, 500, 500)
     this.setState({dataUrl: '', x: 0, y: 0})
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.rafId)
+    if (this.analyser && this.source) {
+      this.analyser.disconnect()
+      this.source.disconnect()
+    }
   }
 
   render() {
